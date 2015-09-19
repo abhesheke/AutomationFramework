23bb8db91fa4a5a192f6b77cbf68beac6cb3fb98
aabhesheke <arjun.abhesheke@compugain.com>
2015-09-19 23:44:18 +0530

    Added url for Alert page

--- FlipKart/src/test/java/com/flipkart/setup/Setup.java
+++ FlipKart/src/test/java/com/flipkart/setup/Setup.java
@@ -11,5 +11,6 @@ public interface Setup {
 
 	public static final String sURL="http://www.flipkart.com";
 	public static final String sbrowsername="Firefox";
+	public static final String sAlertBox="http://www.w3schools.com/js/tryit.asp?filename=tryjs_alert";
 	
 }
